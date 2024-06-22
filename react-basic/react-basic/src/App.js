93e8b4879a9faf07ea1429b3905509f6b4962a20
siri906 <sirirang906@gmail.com>
2024-06-22 12:35:35 +0900

    app base

--- react-basic/react-basic/src/App.js
+++ react-basic/react-basic/src/App.js
@@ -1,35 +1,3 @@
-import { useState } from "react";
-
 export default function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const onChange = (event) => {
-    setTodo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
-  };
-
-  return (
-    <div>
-      <h1>my To Dos {todos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" onChange={onChange} value={todo} />
-        <button type="" onClick={onSubmit}>
-          Add todos
-        </button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <>base</>;
 }
