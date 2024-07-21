1696fe04ba60a02644d5c43249e2fe3cc9517877
siri906 <sirirang906@gmail.com>
2024-07-21 16:26:12 +0900

    fetching 동시에 하는 방법

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,30 @@
 import { Metadata } from "next";
+import Link from "next/link";
 import React from "react";
 
 export const metadata: Metadata = {
   title: "home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovie() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const data = fetch(URL).then((resultData) => resultData.json());
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  const data = fetch(API_URL).then((resultData) => resultData.json());
   return data;
 }
 
 export default async function Home() {
   const movies = await getMovie();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movieItem) => {
+        return (
+          <li key={movieItem.id}>
+            <Link href={`/movie/${movieItem.id}`}>{movieItem.title}</Link>
+          </li>
+        );
+      })}
+    </div>
+  );
 }
--- app/(movies)/movie/[id]/page.tsx
+++ app/(movies)/movie/[id]/page.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { API_URL } from "../../../(home)/page";
 
-export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
-  return <div>movie {id}</div>;
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  const res = await fetch(`${API_URL}/${id}`);
+  return res.json();
+}
+
+async function getVideo(id: string) {
+  console.log(`Fetching video: ${Date.now()}`);
+  const res = await fetch(`${API_URL}/${id}/videos`);
+  return res.json();
+}
+
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  //이렇게 작성하면 병렬 적으로 데이터를 가져오게 되어 버린다
+  // console.log('start fetching');
+  // // const movie = await getMovie(id);
+  // // const video = await getVideo(id);
+  // console.log('end fetching');
+
+  //동시에 가져오려면 Promise.all을 사용한다
+  console.log("start fetching");
+  // 이상태는 영화, 비디오 데이터를 둘다 가져와야지만 변수 사용이 가능하다 둘중 하나라도 완료되면 사용 할 수 있도록 변경
+  const [movie, video] = await Promise.all([getMovie(id), getVideo(id)]);
+  console.log("end fetching");
+  return <div>{movie.title}</div>;
 }
